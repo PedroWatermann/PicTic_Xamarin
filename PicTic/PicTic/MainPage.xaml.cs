89153c7acf6be0af9df28cd2ce9111cfeaec6b81
PedroWatermann <pedro.programmer.pt@gmail.com>
2025-02-28 21:08:42 -0300

    App finalizado!

---
+++ PicTic/PicTic/MainPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using System.IO;
+
+namespace PicTic
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnFoto_Clicked(object sender, EventArgs e)
+        {
+            FileResult result = await MediaPicker.CapturePhotoAsync(); // Armazena a foto
+
+            if (result != null)
+            {
+                Stream stream = await result.OpenReadAsync(); // Lê o último arquivo gravado
+                imgFoto.Source = ImageSource.FromStream(() => stream); // Mostra a foto
+            }
+        }
+
+        private async void btnPegar_Clicked(object sender, EventArgs e)
+        {
+            FileResult result = await MediaPicker.PickPhotoAsync(new MediaPickerOptions // Abre uma caixa de diálogo para selecionar uma foto
+            {
+                Title = "Escolha uma foto..."
+            });
+
+            if (result != null)
+            {
+                Stream stream = await result.OpenReadAsync();
+                imgFoto.Source = ImageSource.FromStream(() => stream);
+            }
+        }
+    }
+}
